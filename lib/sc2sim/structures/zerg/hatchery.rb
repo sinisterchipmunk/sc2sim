8f91bc55cf50dde585725e1f3b3c11b02f0966b1
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-02-16 17:15:43 -0500

    Larva spawn rate corrected to every 15 seconds instead of 14 seconds

--- lib/sc2sim/structures/zerg/hatchery.rb
+++ lib/sc2sim/structures/zerg/hatchery.rb
@@ -5,7 +5,9 @@ class SC2::Structures::Hatchery < SC2::Structures::Base
   build_time 100.seconds
   produces :larva
   
-  every 14.seconds do
+  every 15.seconds do
+    # we can't just build(:larva) because that sticks it in the action queue. Larvae spawn
+    # passively so we should bypass "actions" if for no other reason than the implications.
     army.push(SC2::Units::Larva.new) unless larvae.count >= hatcheries.count * 3
   end
 end
